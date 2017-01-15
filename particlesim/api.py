506dc2b9d2e47874034cdbe83eca950c691a7795
Simon Treu <okrasi@posteo.de>
2017-01-15 20:36:33 +0100

    Added pytest

---
+++ particlesim/api.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def create_new_simulation_configuration():
+    pass
---
+++ particlesim/test_api.py
@@ -0,0 +1,5 @@
+import numpy as np
+from .api import create_new_simulation_configuration
+
+def test_create_new_simulation_configuration_callable():
+    assert False
