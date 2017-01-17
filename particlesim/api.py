36cb0d6a7e19be22e88de20f3a12e5b005b9896a
Jaap Pedersen <jaappedersen@hotmail.de>
2017-01-17 12:45:48 +0100

    started SystemConfiguration, test value type of potential

--- particlesim/api.py
+++ particlesim/api.py
@@ -1,7 +1,33 @@
 import numpy as np
+class TotalPotential(object):
+    # dummy Klasse fuer totales Potential, später ersetzen...
+    def __init__(self, system_configuration):
+        pass
+    pass
+
 
 class SystemConfiguration(object):
-    def potential(self,i,j):
+    r"""
+    A class to save the system configuration parameters
+
+    Parameter:
+        epsilon_r   :   optional, float, default=1.0
+                        Permittivity in medium, default in vacuum
+        box_size    :   optional, float
+                        shape of box, boundaries
+
+    """
+
+    def __init__(self, box_size,epsilon_r=1.0):
+        self.box_size = box_size
+        self.epsilon_r = epsilon_r
+        pass
+    def add_particles_same_type(self, xyz, charge, sigma, epsilon, lj_cutoff):
+        pass
+
+
+    def potential(self):
+        total_potential=TotalPotential(self)
         return 0
     pass
 
@@ -10,3 +36,4 @@ class Sampler(object):
     def marcov_mc(self, system_configuration):
         pass
     pass
+
--- particlesim/test_api.py
+++ particlesim/test_api.py
@@ -6,6 +6,12 @@ def test_sampler_mmc_returns_trajectory():
     # assert sampler.mmc returns an instance of trajectory
     sampler = Sampler()
     iteration_number = 1000
-    system_configuration = SystemConfiguration(box_shape, particles) # kann man mock für Klasse definieren
+    system_configuration = SystemConfiguration() # kann man mock für Klasse definieren
     mmc_result = sampler.marcov_mc(iteration_number, system_configuration)
     assert mmc_result.isinstance(Trajectory)
+
+def test_system_configuration_potential_value():
+    # assert systemconfiguration.potential returns value type float
+    system_configuration = SystemConfiguration()
+    potential = system_configuration.potential()
+    assert isinstance(potential,(float,int))
