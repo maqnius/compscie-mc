4995997a18b412955e2eca0a584d44c0c35623d6
Mark Niehues <niehues.mark@gmail.com>
2017-01-19 17:31:20 +0100

    Ewald test

--- particlesim/ewald_summation.py
+++ particlesim/ewald_summation.py
@@ -1,8 +1,36 @@
+#   particlesim
+#   Copyright (C) 2017 Mark Niehues, Stefaan Hessmann, Jaap Pedersen, Simon Treu
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import numpy as np
+
 class Ewald_Summation(object):
-	"""Was geht ab"""
+    """
+    Calculates system energy using Ewald Summation
+    """
 
-	def __init__(self):
-		pass
+    def __init__(self, system_conf):
+        self.system_conf = system_conf
 
-	def calc_potential(self):
-		pass
+    def long_rangepotential():
+        """
+        Calculaties the long-range potential of a system using Ewald Summation.
+
+        Returns
+        -------
+        float energy
+        """
+        pass
---
+++ particlesim/test_ewald_summation.py
@@ -0,0 +1,41 @@
+#   particlesim
+#   Copyright (C) 2017 Mark Niehues, Stefaan Hessmann, Jaap Pedersen, Simon Treu
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import numpy as np
+from .ewald_summation import Ewald_Summation
+
+
+def test_create_Ewald_Summation():
+    system_conf = test_conf()
+    ewald = Ewald_Summation(system_conf)
+    assert Ewald_Summation.system_conf == "some conf"
+
+
+def test_energy_is_float():
+    system_conf = test_conf()
+    ewald = Ewald_Summation(system_conf)
+    assert isinstance(Ewald_Summation.long_rangepotential(), float)
+
+
+def test_energy_is_positive():
+    system_conf = test_conf()
+    ewald = Ewald_Summation(system_conf)
+    assert Ewald_Summation.long_rangepotential() >= 0.
+
+
+def test_conf():
+    return "some conf"
