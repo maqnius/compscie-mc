d487408c2aa992eccc58ff9a5739abda9c8908ef
Simon Treu <okrasi@posteo.de>
2017-01-21 17:35:35 +0100

    Jaap, Simon:  Function for adding particles created

--- particlesim/api.py
+++ particlesim/api.py
@@ -21,13 +21,55 @@ class SystemConfiguration(object):
         box_size    :   optional, float
                         shape of box, boundaries
 
+    arithmetic mean for sigma and geometric mean for epsilon
+    arithmetic = (a+b)/2; geometric : sqrt(a*a)
+    Lorentz Berthelot Rule
+    lj_cutoff = 2.5 * sigma
     """
-    def __init__(self, box_size=1, epsilon_r=1.0):
+    def __init__(self, box_size=1.0, epsilon_r=1.0):
         self.box_size = box_size
         self.epsilon_r = epsilon_r
+        self.xyz = np.ndarray(shape=(0,3),dtype=float)
+        self.charges = []
+        self.sigmas = []
+        self.epsilons = []
 
-    def add_particles_same_type(self, xyz, charge, sigma, epsilon, lj_cutoff):
-        pass
+    def add_particles(self, xyz, charges, sigmas, epsilons):
+        r"""
+        :param xyz: array(n,3) positions of the particles
+        :param charges: array(n,1)
+        :param sigmas: array(n,1)
+        :param epsilons: array(n,1)
+        :return: nil
+
+        adds the particles to the systemConfiguration
+        """
+        if not len(xyz) == len(charges):
+            raise TypeError('charges must have the same length as particle numbers')
+        if not len(xyz) == len(sigmas):
+            raise TypeError('sigmas must have the same length as particle numbers')
+        if not len(xyz) == len(epsilons):
+            raise TypeError('epsilons must have the same length as particle numbers')
+        self.xyz = np.concatenate((self.xyz, xyz), axis=0)
+        self.charges += charges
+        self.sigmas += sigmas
+        self.epsilons += epsilons
+
+    def add_particles_same_type(self, xyz, charge = 0., sigma = 1.0, epsilon = 1.0):
+        r"""
+        Add particles with same values for charge, sigma and epsilon to the system configuration
+        :param xyz: np.ndarray(n,3)
+        :param charge: float, Default = 0
+        :param sigma: float, Default = 0
+        :param epsilon: float, Default = 0
+        :return:
+
+        """
+        number_of_particles = len(xyz)
+        self.xyz = np.concatenate((self.xyz, xyz), axis=0)
+        self.charges += [charge]*number_of_particles
+        self.sigmas += [sigma]*number_of_particles
+        self.epsilons += [epsilon]*number_of_particles
 
     def potential(self):
         # TODO only stub
@@ -35,8 +77,7 @@ class SystemConfiguration(object):
         return total_potential.potential()
 
     def number_of_particle_types(self):
-        # TODO only stub
-        return 0
+        return len(self.xyz)
 
 
 class Sampler(object):
--- particlesim/test_api.py
+++ particlesim/test_api.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from .api import *
 
 #
@@ -20,13 +21,32 @@ def test_system_configuration_potential_value():
 
 def add_basic_random_particle_group_to_system_config(system_configuration):
     number_of_particles = 100
-    particle_positions = np.random.rand(number_of_particles, 3)
-    charge = np.array([1] * number_of_particles)
-    system_configuration.add_particles_same_type(particle_positions, charge, sigma=1, epsilon=1, lj_cutoff=3)
+    particle_positions = np.random.rand(number_of_particles,3)
+    charge = [1] * number_of_particles
+    sigma = [1.]*50 + [1.5] * 50
+    system_configuration.add_particles(xyz=particle_positions, charges=charge, sigmas=sigma, epsilons=sigma)
 
 
-def test_add_particles_same_type():
+def test_add_particles():
     system_configuration = SystemConfiguration()
     add_basic_random_particle_group_to_system_config(system_configuration)
-    assert 0 < system_configuration.number_of_particle_types()
+    add_basic_random_particle_group_to_system_config(system_configuration)
+    assert 200 == system_configuration.number_of_particle_types()
+
+def test_add_particles_of_same_type():
+    system_configuration = SystemConfiguration()
+    number_of_particles = 100
+    particle_positions = np.random.rand(number_of_particles,3)
+    system_configuration.add_particles_same_type(particle_positions)
+    assert number_of_particles == system_configuration.number_of_particle_types()
+    assert system_configuration.epsilons == [1.0]*number_of_particles
+
+def test_add_particles_not_matching_input():
+    system_configuration = SystemConfiguration()
+    number_of_particles = 100
+    particle_positions = np.random.rand(number_of_particles,3)
+    charge = [1] * 10
+    sigma = [1.]*50 + [1.5] * 50
+    with pytest.raises(TypeError):
+        system_configuration.add_particles(xyz=particle_positions, charges=charge, sigmas=sigma, epsilons=sigma)
 
