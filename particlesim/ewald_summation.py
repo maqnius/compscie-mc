f7612bf51c1b6bda939101aea4687a14857fdac9
Stefaan <Goldstijn@Frankos-MacBook-Pro-2.local>
2017-01-30 18:33:29 +0100

    small change

--- particlesim/ewald_summation.py
+++ particlesim/ewald_summation.py
@@ -13,7 +13,6 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from __future__ import division
 
 import numpy as np
 import scipy.constants
