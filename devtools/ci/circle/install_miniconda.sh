687feb36d6d883007abed81dfb00c59da118fd04
Simon Treu <okrasi@posteo.de>
2017-02-03 20:45:24 +0100

    use miniconda3 in ci

--- devtools/ci/circle/install_miniconda.sh
+++ devtools/ci/circle/install_miniconda.sh
@@ -7,7 +7,7 @@ function install_miniconda {
     if [ -d $TARGET ]; then echo "file exists"; return; fi
     echo "installing miniconda to $TARGET"
         platform="Linux"
-    wget http://repo.continuum.io/miniconda/Miniconda2-latest-$platform-x86_64.sh -O mc.sh -o /dev/null
+    wget http://repo.continuum.io/miniconda/Miniconda3-latest-$platform-x86_64.sh -O mc.sh -o /dev/null
     bash mc.sh -b -f -p $TARGET
 }
 
