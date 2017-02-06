26e4dcaa822789318724e285cb6d7051a57fe73a
Mark Niehues <niehues.mark@gmail.com>
2017-02-06 20:55:25 +0100

    basic config reader

--- particlesim/utils/config_parser.py
+++ particlesim/utils/config_parser.py
@@ -1,21 +1,83 @@
 import configparser
+import os
 from numpy import genfromtxt
-import csv
+from particlesim.api import SystemConfiguration
 
 class ProblemCreator(object):
     def __init__(self, config_file_path):
-        self.parser = configparser.ConfigParser()
-        self.config = self.parser.read(config_file_path)
+        '''
+        Parameters
+        ----------
+        config_file_path : String
+            Path to the .cfg config file
+        '''
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file_path)
         self.ewald = True
+        self.lennard_jones = True
 
         # Check necessary parameters
-        if(self.config['init_configuration']['csv_path']):
-            path = self.config['init_configuration']['csv_path']
-            self.initial_configuration = genfromtxt(path, delimiter = "\t", skip_header = 2)
+        if(self.config['general']['csv_path']):
+            path = self.config['general']['csv_path']
+            self.initial_configuration = genfromtxt(path, delimiter = ",", skip_header = 2, unpack = True)
+            self.n = len(self.initial_configuration[0])
         else:
             raise ValueError("No link to initial configuration given")
+
+        if(self.config['general']['box-size']):
+            self.box_size = self.config['general'].getfloat('box-size')
+        else:
+            raise ValueError("No box-size given")
 
         if(self.config['ewald_summation']['use_ewald']):
             self.ewald = self.config['ewald_summation'].getboolean('use_ewald')
             if(self.ewald):
-                self.sigma_ewald = self.config['ewald_summation']['sigma']
+                try:
+                    self.sigma_ewald = self.config['ewald_summation']['sigma']
+                except KeyError:
+                    self.ewald= False
+                    self.config['ewald_summation']['use_ewald'] = "no"
+                    print("No sigma for Ewald Summation found therefore Ewald summation got deactivated.")
+
+        self.lennard_jones = self.config['lennard_jones'].getboolean('use_lennard_jones')
+
+    def generate_problem(self):
+        # Unfiddle the input csv file
+        positions = self.initial_configuration[:3].transpose()
+        charges = self.initial_configuration[3].transpose()
+        epsilons = self.initial_configuration[4].transpose()
+        sigmas = self.initial_configuration[5].transpose()
+
+        self.system_conf = SystemConfiguration(positions, sigmas, epsilons, charges, self.box_size)
+
+        return self.system_conf
+
+    def export_config(self):
+        '''
+        Exports the used config files. There might be differences with the
+        user specified config file because of default parameters.
+
+        The config file will be created in the log folder
+
+        '''
+        parent = (os.path.dirname(os.path.dirname(__file__)))
+        log_dir = os.path.abspath(os.path.join(parent, 'logs/'))
+
+        # Check if logs exists
+        if not os.path.isdir(log_dir):
+            os.makedirs(log_dir)
+
+        # Write to .cfg file
+        config_path = os.path.abspath(os.path.join(log_dir, 'config.cfg'))
+        with open(config_path, 'w') as configfile:
+            self.config.write(configfile)
+
+    def export_output_csv(self):
+        '''
+        Generates an output csv file for the current configuration
+
+        Returns
+        -------
+
+        '''
+        pass
--- particlesim/utils/test_example.py
+++ particlesim/utils/test_example.py
@@ -1,4 +1,31 @@
 from particlesim.utils.config_parser import ProblemCreator
+import os
+import configparser
 
-test_example_configuration():
-    creator = ProblemCreator("/example/example_init_positions.csv")
+def test_example_configuration():
+    test_config_path = create_test_config()
+    creator = ProblemCreator(test_config_path)
+    system_config = creator.generate_problem()
+    creator.export_config()
+
+def create_test_config():
+    # Find path to example_init_positions.csv
+    # in ../../example/
+    parent = os.path.dirname((os.path.dirname(os.path.dirname(__file__))))
+    csv_path = os.path.abspath(os.path.join(parent, 'example/example_init_positions.csv'))
+
+    # Create example configuration fields
+    config = configparser.ConfigParser()
+
+    config['DEFAULT'] = {'use_ewald': 'yes', 'use_lennard_jones': 'yes'}
+
+    config['general'] = {'csv_path': csv_path, 'box-size': 5.0}
+    config['ewald_summation'] = {'use_ewald': 'yes', 'sigma': 1.0}
+    config['lennard_jones'] = {'use_lennard_jones': 'yes'}
+
+    # Write to .cfg file
+    config_path = os.path.abspath(os.path.join(parent, 'example/example_config.cfg'))
+    with open(config_path, 'w') as configfile:
+        config.write(configfile)
+
+    return config_path
