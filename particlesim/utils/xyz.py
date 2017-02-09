c4016da147e4bfab66e028042c862249257b11ad
Mark Niehues <niehues.mark@gmail.com>
2017-02-09 21:02:32 +0100

    xyz-export

--- particlesim/utils/xyz.py
+++ particlesim/utils/xyz.py
@@ -1,5 +1,30 @@
-def write():
-    pass
+import numpy as np
 
-def read():
-    pass
+def export_trajectory(labels, trajectory, path):
+    """
+    Exports a trajectory in xyz format
+
+    Parameters
+    ----------
+    labels : list
+        A list containing the labels of the atom
+
+    trajectory : list[np.ndarray]
+        A list containing the xyz np.ndarrays
+
+    path : str
+        The path to the export file
+    """
+
+    n = len(labels)
+    labels = np.array(labels).reshape((30,1))
+
+    with open(path, 'wb') as file:
+        for xyz in trajectory:
+            file.write(str.encode("%s\n" %n))
+            file.write(str.encode("label x y z\n"))
+
+            with_labels = np.concatenate((labels, xyz), axis=1)
+            np.savetxt(file, with_labels, delimiter=" ", fmt = '%s')
+
+            file.write(str.encode("\n"))
