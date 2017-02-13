3371cdd96f2e7335d2488666608b484e81d9a8ef
Stefaan <hessmann.stefaan@googlemail.com>
2017-02-13 16:21:43 +0100

    all testfunctions moved to single file

--- particlesim/test_total_potential.py
+++ particlesim/test_total_potential.py
@@ -14,13 +14,18 @@
 #   You should have received a copy of the GNU General Public License
 #
 
+
 import numpy as np
 from scipy.special import erfc
-from particlesim.utils.conversion import prefactor
 from .helpers_for_tests import create_system_configuration
 from .helpers_for_tests import create_positions
+from particlesim.utils.conversion import prefactor
 from .api import SystemConfiguration
+from .ewald_summation import EwaldSummation
+from .shortrange import Shortrange
 from .total_potential import TotalPotential
+
+
 
 def test_longrange_potential_is_float():
     n = 100
@@ -94,6 +99,116 @@ def test_shortrange_coulomb_with_4_charges():
     shortrange_pot *= 1/(4*np.pi) * prefactor
     shortrange_pot_sim = potential.shortrange_energy(xyz, lennard_jones=False)
     np.testing.assert_almost_equal(actual=shortrange_pot_sim, desired=shortrange_pot, decimal=5)
+
+
+def test_shortrange_ewald():
+    """
+    Test Coulomb energy by comparing it to a simple test-system.
+    """
+    ewald_sigma = 1.0
+    q = 2.0
+    boxsize = 10.
+    charges = np.array([q, -1 * q])
+
+    xyz = np.array([[0., 0., 0.], [0., 0., 1.]])
+    system_conf = SystemConfiguration(xyz=xyz, charges=charges, box_size=boxsize, neighbouring=False)
+    shortrange = Shortrange(system_conf,sigma_c = ewald_sigma, r_cutoff=2.)
+    theoretical_shortrange_energy = -4 * erfc(1/(np.sqrt(2)*ewald_sigma))
+    shortrange_energy = shortrange.shortrange(xyz, lj=False)
+
+    # Test the pbc
+    xyz = np.array([[0., 0., 0.], [0., 0., 9.]])
+    system_conf = SystemConfiguration(xyz=xyz, charges=charges, box_size=boxsize, neighbouring= False)
+    shortrange = Shortrange(system_conf, sigma_c=ewald_sigma, r_cutoff=2.)
+    shortrange_energy_periodic = shortrange.shortrange(xyz, lj=False)
+
+
+    np.testing.assert_almost_equal(actual=shortrange_energy, desired=theoretical_shortrange_energy*1/(4*np.pi) * prefactor, decimal=5)
+    np.testing.assert_almost_equal(actual=shortrange_energy_periodic, desired=theoretical_shortrange_energy*1/(4*np.pi) * prefactor, decimal=5)
+
+
+
+def test_lj_potential():
+    """
+    Test Lennard-Jones energy by comparing it to a simple test-system.
+    """
+    ewald_sigma = 1.
+    epsilon_Na = 0.0469
+    epsilon_Cl = 0.15
+    epsilon_NaCl = np.sqrt(epsilon_Cl*epsilon_Na)
+    epsilons = np.array([epsilon_Na, epsilon_Cl])
+
+    sigma_Na = 1.21496
+    sigma_Cl = 2.02234
+    sigma_NaCl = 0.5*(sigma_Na + sigma_Cl)
+    sigmas = np.array([sigma_Na, sigma_Cl])
+
+    xyz = np.array([[0., 0., 0.], [0., 0., 1.]])
+    charges = np.array([1., -1.])
+    boxsize = 12.
+
+    thoeretical_lj_energy = 4 * epsilon_NaCl * (sigma_NaCl**12 - sigma_NaCl**6)
+
+    system_conf = SystemConfiguration(xyz=xyz, charges=charges, box_size=boxsize, sigmas=sigmas, epsilons=epsilons, neighbouring=False)
+    shortrange = Shortrange(system_conf, sigma_c=ewald_sigma, r_cutoff=2.)
+    shortrange_energy = shortrange.shortrange(xyz, coulomb=False)
+
+    np.testing.assert_almost_equal(actual=shortrange_energy, desired=thoeretical_lj_energy, decimal=5)
+
+
+
+def test_longrange_potential():
+    """
+    Test longrange Energy by comparing it to a simple test-system.
+    """
+    xyz = np.array([[0., 0., 0.], [0., 0., 1.]])
+    charges = np.array([1., -1.])
+    boxsize = 6.0
+    sigma = 1.
+    k_cutoff = 1.
+
+    system_conf = SystemConfiguration(xyz=xyz, charges=charges, box_size=boxsize)
+    ewald_summation = EwaldSummation(system_conf, sigma=sigma, k_cutoff=k_cutoff)
+
+    theoretical_longrange_energy = 1/(boxsize*2*np.pi) * np.exp(-sigma**2*(2*np.pi/boxsize)**2 / 2) *\
+                                   (np.abs(1-np.exp(1j*2*np.pi/boxsize))**2 + np.abs(1-np.exp(-1j*2*np.pi/boxsize))**2)
+    theoretical_self_energy = 2/(np.sqrt(2*np.pi)*sigma)
+
+    theoretical_total_energy = theoretical_longrange_energy - theoretical_self_energy
+
+    simulated_longrange_energy = ewald_summation.longrange_energy(xyz)
+
+    np.testing.assert_almost_equal(actual=simulated_longrange_energy, desired=theoretical_total_energy * 1/(4*np.pi) * prefactor, decimal=5)
+
+
+
+def test_total_potential():
+    """
+    Testfunction for total potential
+    """
+    ewald_sigma = 1.
+    epsilon_Na = 0.0469
+    epsilon_Cl = 0.15
+    epsilon_NaCl = np.sqrt(epsilon_Cl * epsilon_Na)
+    epsilons = np.array([epsilon_Na, epsilon_Cl])
+
+    sigma_Na = 1.21496
+    sigma_Cl = 2.02234
+    sigma_NaCl = 0.5 * (sigma_Na + sigma_Cl)
+    sigmas = np.array([sigma_Na, sigma_Cl])
+
+    xyz = np.array([[0., 0., 0.], [0., 0., 1.]])
+    charges = np.array([1., -1.])
+    boxsize = 12.
+
+    system_conf = SystemConfiguration(xyz=xyz, charges=charges, box_size=boxsize, sigmas=sigmas, epsilons=epsilons, k_cutoff = 10, r_cutoff = 4)
+    total_pot = TotalPotential(system_conf)
+    total_pot.sigma_c = 1.
+    theoretical_potential = - 1/(np.pi * 4 ) * prefactor + 4 * epsilon_NaCl * (sigma_NaCl**12 - sigma_NaCl**6)
+
+    potential = total_pot.potential(xyz)
+
+    np.testing.assert_allclose(actual=potential, desired=theoretical_potential, rtol=0.3)
 
 
 
